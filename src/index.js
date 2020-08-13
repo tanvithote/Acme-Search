8503bd47fbdf21188d17d5434725097253f67bb3
tthote <tthote@iu.edu>
2020-08-13 16:44:32 -0400

    Implemented basic requirements

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import JavascriptTimeAgo from 'javascript-time-ago'
+ 
+import en from 'javascript-time-ago/locale/en'
+import ru from 'javascript-time-ago/locale/ru'
+ 
+JavascriptTimeAgo.addLocale(en)
+JavascriptTimeAgo.addLocale(ru)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
