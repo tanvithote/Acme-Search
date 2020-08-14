a43c5cc5d8effa000034ba3bba39eaf6b37e8017
Rajas Deshpande <rajasdeshpande27@gmail.com>
2020-08-14 01:03:57 -0500

    changed filter function in handleClick

--- src/results.js
+++ src/results.js
@@ -3,20 +3,20 @@ import './results.css';
 import Timestamp from 'react-timestamp';
 import ReactTimeAgo from 'react-time-ago';
 
-class Results extends Component{
+class Results extends Component {
     constructor(props) {
-        super(props);  
+        super(props);
         this.state = {
-          calendar: [],
-          contacts: [],
-          dropbox: [],
-          slack: [],
-          tweet: [],
+            calendar: [],
+            contacts: [],
+            dropbox: [],
+            slack: [],
+            tweet: [],
 
         };
-      }
+    }
 
-      componentDidMount() {
+    componentDidMount() {
         const result_calendar = this.props.calendar.sort(
             (a, b) => parseFloat(b.match_value) - parseFloat(a.match_value)
         );
@@ -35,32 +35,36 @@ class Results extends Component{
         );
 
 
-        this.setState({calendar: result_calendar, contacts: result_contacts, dropbox: result_dropbox, slack: result_slack, tweet: result_tweet})
-      }
+        this.setState({ calendar: result_calendar, contacts: result_contacts, dropbox: result_dropbox, slack: result_slack, tweet: result_tweet })
+    }
 
-      renderCalendar = calendar => {
+    renderCalendar = calendar => {
         return (
-            <div class="row">
-                {calendar.map((c,i) => {
+            <div className="row">
+                {calendar.map((c, i) => {
                     // console.log(calendar[i+1])
                     return (
-                        <div class="card text-center">
-                        <div class="card-header">
-                            <b style={{display:(i===0 && c.match_value>1) || 
-                                (i>0 && c.match_value>1 && c.match_value===calendar[i-1].match_value)?"":"none", color:"red"}}>Best Match</b>
-                            <br style={{display:(i===0 && c.match_value>1) || 
-                                (i>0 && c.match_value>1 && c.match_value===calendar[i-1].match_value)?"":"none", color:"red"}}/>
+                        <div className="card text-center">
+                            <div className="card-header">
+                                <b style={{
+                                    display: (i === 0 && c.match_value > 1) ||
+                                        (i > 0 && c.match_value > 1 && c.match_value === calendar[i - 1].match_value) ? "" : "none", color: "red"
+                                }}>Best Match</b>
+                                <br style={{
+                                    display: (i === 0 && c.match_value > 1) ||
+                                        (i > 0 && c.match_value > 1 && c.match_value === calendar[i - 1].match_value) ? "" : "none", color: "red"
+                                }} />
                             Title : {c.title}
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Invitees: </h5>
-                            <p class="card-text">{c.invitees}</p>
-                        </div>
-                        <div class="card-footer text-muted">
-                            Date: {<Timestamp date={c.date} options={{ includeDay: true}} />
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">Invitees: </h5>
+                                <p className="card-text">{c.invitees}</p>
+                            </div>
+                            <div className="card-footer text-muted">
+                                Date: {<Timestamp date={c.date} options={{ includeDay: true }} />
                                 }
+                            </div>
                         </div>
-                    </div>
                     );
                 })}
             </div>
@@ -69,43 +73,45 @@ class Results extends Component{
 
     renderContacts = contacts => {
         return (
-            <div class="row">
-                {contacts.map((c,i) => {
+            <div className="row">
+                {contacts.map((c, i) => {
                     return (
-                        <div class="card text-center">
-                        <div class="card-header">
-                            <b style={{display:(i===0 && c.match_value>1) || 
-                                (i>0 && c.match_value>1 && c.match_value===contacts[i-1].match_value)?"":"none", color:"red"}}>Best Match</b>
-                            <br style={{display:(i===0 && c.match_value>1) || 
-                                (i>0 && c.match_value>1 && c.match_value===contacts[i-1].match_value)?"":"none", color:"red"}}/>
+                        <div className="card text-center">
+                            <div className="card-header">
+                                <b style={{
+                                    display: (i === 0 && c.match_value > 1) ||
+                                        (i > 0 && c.match_value > 1 && c.match_value === contacts[i - 1].match_value) ? "" : "none", color: "red"
+                                }}>Best Match</b>
+                                <br style={{
+                                    display: (i === 0 && c.match_value > 1) ||
+                                        (i > 0 && c.match_value > 1 && c.match_value === contacts[i - 1].match_value) ? "" : "none", color: "red"
+                                }} />
                             Name : {c.name}
-                            <br/>
+                                <br />
                             Company: {c.company}
-                        </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Email: </h5>
-                            {c.emails.map((email)=>
-                                {
-                                    return(
-                                        <p class="card-text">{email}</p>
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">Email: </h5>
+                                {c.emails.map((email) => {
+                                    return (
+                                        <p className="card-text">{email}</p>
                                     )
                                 }
-                            )}
-                            
-                            <h5 class="card-title">Phone: </h5>
-                            {c.phones.map((phone)=>
-                                {
-                                    return(
-                                        <p class="card-text">{phone}</p>
+                                )}
+
+                                <h5 className="card-title">Phone: </h5>
+                                {c.phones.map((phone) => {
+                                    return (
+                                        <p className="card-text">{phone}</p>
                                     )
                                 }
-                            )}
-                        </div>
-                        <div class="card-footer text-muted">
-                            Last Contacted: {<ReactTimeAgo date={c.last_contact}/>
+                                )}
+                            </div>
+                            <div className="card-footer text-muted">
+                                Last Contacted: {<ReactTimeAgo date={c.last_contact} />
                                 }
+                            </div>
                         </div>
-                    </div>
                     );
                 })}
             </div>
@@ -114,27 +120,31 @@ class Results extends Component{
 
     renderDropbox = dropbox => {
         return (
-            <div class="row">
-                {dropbox.map((d,i) => {
+            <div className="row">
+                {dropbox.map((d, i) => {
                     return (
-                        <div class="card text-center">
-                        <div class="card-header">
-                            <b style={{display:(i===0 && d.match_value>1) || 
-                                (i>0 && d.match_value>1 && d.match_value===dropbox[i-1].match_value)?"":"none", color:"red"}}>Best Match</b>
-                            <br style={{display:(i===0 && d.match_value>1) || 
-                                (i>0 && d.match_value>1 && d.match_value===dropbox[i-1].match_value)?"":"none", color:"red"}}/>
+                        <div className="card text-center">
+                            <div className="card-header">
+                                <b style={{
+                                    display: (i === 0 && d.match_value > 1) ||
+                                        (i > 0 && d.match_value > 1 && d.match_value === dropbox[i - 1].match_value) ? "" : "none", color: "red"
+                                }}>Best Match</b>
+                                <br style={{
+                                    display: (i === 0 && d.match_value > 1) ||
+                                        (i > 0 && d.match_value > 1 && d.match_value === dropbox[i - 1].match_value) ? "" : "none", color: "red"
+                                }} />
                             Title : {d.title}
-                            <br/>
+                                <br />
                             Path: {d.path}
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">Shared with: </h5>
+                                <p className="card-text">{d.shared_with}</p>
+                            </div>
+                            <div className="card-footer text-muted">
+                                Created: {<ReactTimeAgo date={d.created} />}
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Shared with: </h5>
-                            <p class="card-text">{d.shared_with}</p>
-                        </div>
-                        <div class="card-footer text-muted">
-                            Created: {<ReactTimeAgo date={d.created}/>}
-                        </div>
-                    </div>
                     );
                 })}
             </div>
@@ -143,89 +153,99 @@ class Results extends Component{
 
     renderSlack = slack => {
         return (
-            <div class="row">
-                {slack.map((s,i) => {
+            <div className="row">
+                {slack.map((s, i) => {
                     return (
-                        <div class="card text-center">
-                        <div class="card-header">
-                            <b style={{display:(i===0 && s.match_value>1) || 
-                                (i>0 && s.match_value>1 && s.match_value===slack[i-1].match_value)?"":"none", color:"red"}}>Best Match</b>
-                            <br style={{display:(i===0 && s.match_value>1) || 
-                                (i>0 && s.match_value>1 && s.match_value===slack[i-1].match_value)?"":"none", color:"red"}}/>
+                        <div className="card text-center">
+                            <div className="card-header">
+                                <b style={{
+                                    display: (i === 0 && s.match_value > 1) ||
+                                        (i > 0 && s.match_value > 1 && s.match_value === slack[i - 1].match_value) ? "" : "none", color: "red"
+                                }}>Best Match</b>
+                                <br style={{
+                                    display: (i === 0 && s.match_value > 1) ||
+                                        (i > 0 && s.match_value > 1 && s.match_value === slack[i - 1].match_value) ? "" : "none", color: "red"
+                                }} />
                             Channel : {s.channel}
-                            <br/>
+                                <br />
                             Author: {s.author}
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">Message: </h5>
+                                <p className="card-text">{s.message}</p>
+                            </div>
+                            <div className="card-footer text-muted">
+                                Time: {<Timestamp date={s.timestamp} options={{ includeDay: true }} />}
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Message: </h5>
-                            <p class="card-text">{s.message}</p>
-                        </div>
-                        <div class="card-footer text-muted">
-                            Time: {<Timestamp date={s.timestamp} options={{ includeDay: true}} />}
-                        </div>
-                    </div>
                     );
                 })}
             </div>
         );
     };
-    
+
     renderTweet = tweet => {
         return (
-            <div class="row">
-                {tweet.map((s,i) => {
+            <div className="row">
+                {tweet.map((s, i) => {
                     return (
-                        <div class="card text-center">
-                        <div class="card-header">
-                            <b style={{display:(i===0 && s.match_value>1) || 
-                                (i>0 && s.match_value>1 && s.match_value===tweet[i-1].match_value)?"":"none", color:"red"}}>Best Match</b>
-                            <br style={{display:(i===0 && s.match_value>1) || 
-                                (i>0 && s.match_value>1 && s.match_value===tweet[i-1].match_value)?"":"none", color:"red"}}/>
+                        <div className="card text-center">
+                            <div className="card-header">
+                                <b style={{
+                                    display: (i === 0 && s.match_value > 1) ||
+                                        (i > 0 && s.match_value > 1 && s.match_value === tweet[i - 1].match_value) ? "" : "none", color: "red"
+                                }}>Best Match</b>
+                                <br style={{
+                                    display: (i === 0 && s.match_value > 1) ||
+                                        (i > 0 && s.match_value > 1 && s.match_value === tweet[i - 1].match_value) ? "" : "none", color: "red"
+                                }} />
                             User : {s.user}
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">Message: </h5>
+                                <p className="card-text">{s.message}</p>
+                            </div>
+                            <div className="card-footer text-muted">
+                                Time: {s.timestamp}
+                            </div>
                         </div>
-                        <div class="card-body">
-                            <h5 class="card-title">Message: </h5>
-                            <p class="card-text">{s.message}</p>
-                        </div>
-                        <div class="card-footer text-muted">
-                            Time: {s.timestamp}
-                        </div>
-                    </div>
                     );
                 })}
             </div>
         );
     };
-    render(){
-        const {calendar, contacts, dropbox, slack, tweet} = this.state;
-        return(
+    render() {
+        const { calendar, contacts, dropbox, slack, tweet } = this.state;
+        return (
             <div>
-                <div class="no_results" style={{display:calendar.length===0 &&
-                contacts.length===0 &&
-                dropbox.length===0 &&
-                slack.length===0 &&
-                tweet.length===0
-                ? "":"none"}}>
-                No Results found! Please search for something else.
+                <div className="no_results" style={{
+                    display: calendar.length === 0 &&
+                        contacts.length === 0 &&
+                        dropbox.length === 0 &&
+                        slack.length === 0 &&
+                        tweet.length === 0
+                        ? "" : "none"
+                }}>
+                    No Results found! Please search for something else.
 
                 </div>
-                <div class="calendar" style={{display:calendar.length>0? "":"none"}}>
+                <div className="calendar" style={{ display: calendar.length > 0 ? "" : "none" }}>
                     <h6>Calendar Results:</h6>
                     {this.renderCalendar(calendar)}
                 </div>
-                <div class="calendar" style={{display:contacts.length>0? "":"none"}}>
+                <div className="calendar" style={{ display: contacts.length > 0 ? "" : "none" }}>
                     <h6>Contact Results:</h6>
                     {this.renderContacts(contacts)}
                 </div>
-                <div class="calendar" style={{display:dropbox.length>0? "":"none"}}>
+                <div className="calendar" style={{ display: dropbox.length > 0 ? "" : "none" }}>
                     <h6>Dropbox Results:</h6>
                     {this.renderDropbox(dropbox)}
                 </div>
-                <div class="calendar" style={{display:slack.length>0? "":"none"}}>
+                <div className="calendar" style={{ display: slack.length > 0 ? "" : "none" }}>
                     <h6>Slack Results:</h6>
                     {this.renderSlack(slack)}
                 </div>
-                <div class="calendar" style={{display:tweet.length>0? "":"none"}}>
+                <div className="calendar" style={{ display: tweet.length > 0 ? "" : "none" }}>
                     <h6>Tweet Results:</h6>
                     {this.renderTweet(tweet)}
                 </div>
--- src/search.js
+++ src/search.js
@@ -9,173 +9,107 @@ import Results from './results';
 
 class Search extends Component {
     constructor(props) {
-      super(props);  
-      this.state = {
-        calendar: calendar,
-        contacts: contacts,
-        dropbox: dropbox,
-        slack: slack,
-        tweet: tweet,
-        query: '',
-        clicked:false,
-        result_calendar: [],
-        result_contacts: [],
-        result_dropbox: [],
-        result_slack: [],
-        result_tweet: [],
-        loading: false,
-        message: '',
-      };
-      this.handleClick = this.handleClick.bind(this);
+        super(props);
+        this.state = {
+            calendar: calendar,
+            contacts: contacts,
+            dropbox: dropbox,
+            slack: slack,
+            tweet: tweet,
+            query: '',
+            clicked: false,
+            result_calendar: [],
+            result_contacts: [],
+            result_dropbox: [],
+            result_slack: [],
+            result_tweet: [],
+            loading: false,
+            message: '',
+        };
+        this.handleClick = this.handleClick.bind(this);
     }
-  
-    handleOnInputChange = (event) => {
-      const query = event.target.value;
-      this.setState({ query, loading: true, message: '',clicked:false  } );
-    //   console.log(query)
-    };
-  
-    handleClick=(event)=>{
-        // event.preventDefault();
-        
-      const query = this.state.query;
-      const calendar = this.state.calendar;
-      const contacts = this.state.contacts;
-      const dropbox = this.state.dropbox;
-      const slack = this.state.slack;
-      const tweet = this.state.tweet;
-      event.stopPropagation();
-      this.setState({result_calendar: calendar.filter(function(c){
-            let val = 0;
-            c.matching_terms.forEach(item=>
-                {
-                    if(query.toLowerCase().indexOf(item.toLowerCase()) !== -1){
-                        val += 1;
-                    }
-                }                
-            )
-            if(val>0){
-                c["match_value"]=val;
-                // console.log(c);
-                return c;
-            }              
-      })
-    });
-    this.setState({clicked: true});
-    this.setState({result_contacts: contacts.filter(function(c){
-        let val=0;
-        c.matching_terms.forEach(item=>
-            {
-                if(query.toLowerCase().indexOf(item.toLowerCase()) !== -1){
-                    val += 1;
-                }
-            }                
-        )  
-        // console.log(c, val)
-        if(val>0){
-            c["match_value"]=val;
-            return c;
-        }        
-    })
-    });
-    this.setState({result_dropbox: dropbox.filter(function(c){
-        let val=0;
-            c.matching_terms.forEach(item=>
-                {
-                    if(query.toLowerCase().indexOf(item.toLowerCase()) !== -1){
-                        val += 1;
-                    }
-                }                
-            ) 
-            // console.log(c, val)
-            if(val>0){
-                c["match_value"]=val;
-                return c;
-            }        
-    })
-    });
-    this.setState({result_slack: slack.filter(function(c){
-        let val=0;
-            c.matching_terms.forEach(item=>
-                {
-                    if(query.toLowerCase().indexOf(item.toLowerCase()) !== -1){
-                        val += 1;
-                    }
-                }                
-            )  
-            // console.log(c, val)
-            if(val>0){
-                c["match_value"]=val;
-                return c;
-            }        
-    })
-    });
-    this.setState({result_tweet: tweet.filter(function(c){
-        let val=0;
-            c.matching_terms.forEach(item=>
-                {
-                    if(query.toLowerCase().indexOf(item.toLowerCase()) !== -1){
-                        val += 1;
-                    }
-                }                
-            )
-            if(val>0){
 
-                c["match_value"]=val;
-                return c;
-            }        
-    })
-    });
-  }
-  
+    handleOnInputChange = (event) => {
+        const query = event.target.value;
+        this.setState({ query, loading: true, message: '', clicked: false });
+        //   console.log(query)
+    };
+
+    handleClick = (event) => {
+        // event.preventDefault();
+        const calendar = this.state.calendar;
+        const contacts = this.state.contacts;
+        const dropbox = this.state.dropbox;
+        const slack = this.state.slack;
+        const tweet = this.state.tweet;
+        event.stopPropagation();
+
+        this.setState({ clicked: true });
+
+        this.setState({
+            result_calendar: calendar.filter(c => c["matching_terms"].includes(this.state.query))
+        });
+
+        this.setState({
+            result_contacts: contacts.filter(c => c["matching_terms"].includes(this.state.query))
+        });
+        this.setState({
+            result_dropbox: dropbox.filter(c => c["matching_terms"].includes(this.state.query))
+        });
+        this.setState({
+            result_slack: slack.filter(c => c["matching_terms"].includes(this.state.query))
+        });
+        this.setState({
+            result_tweet: tweet.filter(c => c["matching_terms"].includes(this.state.query))
+        });
+    }
+
     render() {
         const clicked = this.state.clicked;
         // console.log(clicked);
-      return (
-        <div>
-        <div className="container">
-        <br/>
-        <div className="row justify-content-center">
-          <div className="col-12 col-md-10 col-lg-8">
-              <form className="card card-sm">
-                  <div className="card-body row no-gutters align-items-center">
-                      <div className="col-auto">
-                          <i className="fas fa-search h4 text-body"></i>
-                      </div>
-                      <div className="col">
-                          <input className="form-control form-control-lg form-control-borderless" type="search" placeholder="Search keywords or tags" onChange={this.handleOnInputChange}
-                          onKeyPress={event => {
-                              
-                            if (event.key === 'Enter') {
-                                event.preventDefault(); 
-                              this.handleClick(event)
-                            }
-                          }}
-                          />
-                      </div>
-                      <div className="col-auto">
-                          <button className="btn btn-lg btn-info" type="button" onClick={this.handleClick}>Search</button>
-                      </div> 
-                  </div>
-              </form>
-          </div>
-      </div>
-      </div>
-      <div class="results">
-      {clicked && 
-        <Results calendar={this.state.result_calendar} 
-            dropbox={this.state.result_dropbox} 
-            contacts={this.state.result_contacts} 
-            tweet={this.state.result_tweet} 
-            slack={this.state.result_slack}
-            />
-      }
-      </div>
-      </div>
-  
-      );
+        return (
+            <div>
+                <div className="container">
+                    <br />
+                    <div className="row justify-content-center">
+                        <div className="col-12 col-md-10 col-lg-8">
+                            <form className="card card-sm">
+                                <div className="card-body row no-gutters align-items-center">
+                                    <div className="col-auto">
+                                        <i className="fas fa-search h4 text-body"></i>
+                                    </div>
+                                    <div className="col">
+                                        <input className="form-control form-control-lg form-control-borderless" type="search" placeholder="Search keywords or tags" onChange={this.handleOnInputChange}
+                                            onKeyPress={event => {
+                                                if (event.key === 'Enter') {
+                                                    event.preventDefault();
+                                                    this.handleClick(event)
+                                                }
+                                            }}
+                                        />
+                                    </div>
+                                    <div className="col-auto">
+                                        <button className="btn btn-lg btn-info" type="button" onClick={this.handleClick}>Search</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div className="results">
+                    {clicked &&
+                        <Results calendar={this.state.result_calendar}
+                            dropbox={this.state.result_dropbox}
+                            contacts={this.state.result_contacts}
+                            tweet={this.state.result_tweet}
+                            slack={this.state.result_slack}
+                        />
+                    }
+                </div>
+            </div>
+
+        );
     }
-  }
-  
-  export default Search;
-  
+}
+
+export default Search;
